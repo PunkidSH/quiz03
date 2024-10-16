a096e33cd600d9b57b0c73fd152322d3d1d5c085
PunkidSH <940809psh@naver.com>
2024-10-16 16:16:36 +0900

    1

---
+++ quiz03/src/main/java/bitc/fullstack405/quiz03/Quiz03Application.java
@@ -0,0 +1,13 @@
+package bitc.fullstack405.quiz03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz03Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Quiz03Application.class, args);
+  }
+
+}
---
+++ quiz03/src/test/java/bitc/fullstack405/quiz03/Quiz03ApplicationTests.java
@@ -0,0 +1,13 @@
+package bitc.fullstack405.quiz03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz03ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
